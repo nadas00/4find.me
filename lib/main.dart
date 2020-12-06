c0acc04d0443fb9ffbb7fde6ed042abe7e9cc5f0
nadas00 <se.hasanciftci@gmail.com>
2020-12-06 18:02:56 +0300

    Friends page added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:four_find_me/screens/friends_screen.dart';
 import 'package:four_find_me/screens/login_screen.dart';
 import 'package:four_find_me/screens/profile_screen.dart';
 import 'package:four_find_me/screens/register_screen.dart';
@@ -12,12 +13,13 @@ class FindMe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: ProfileScreen.id,
+      initialRoute: FriendsScreen.id,
       routes: {
         WelcomeScreen.id : (context) => WelcomeScreen(),
         LoginScreen.id : (context) => LoginScreen(),
         RegisterScreen.id : (context) => RegisterScreen(),
         ProfileScreen.id : (context) => ProfileScreen(),
+        FriendsScreen.id: (context) => FriendsScreen(),
       },
     );
   }
